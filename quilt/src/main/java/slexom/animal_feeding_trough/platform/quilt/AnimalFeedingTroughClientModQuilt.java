f77099c8ea421e2dbba53f04f70ae0bdf2970536
Carmelo Buscemi <b.carmelo.92@gmail.com>
2022-06-11 12:06:53 +0200

    feat: quilt

--- quilt/src/main/java/slexom/animal_feeding_trough/platform/quilt/AnimalFeedingTroughClientModQuilt.java
+++ quilt/src/main/java/slexom/animal_feeding_trough/platform/quilt/AnimalFeedingTroughClientModQuilt.java
@@ -1,15 +1,18 @@
 package slexom.animal_feeding_trough.platform.quilt;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
 import slexom.animal_feeding_trough.platform.common.AnimalFeedingTroughMod;
 
 public class AnimalFeedingTroughClientModQuilt implements ClientModInitializer {
+
     @Override
+    @Environment(EnvType.CLIENT)
     public void onInitializeClient(ModContainer mod) {
         AnimalFeedingTroughMod.onInitializeClient();
     }
-
 
 }
 
