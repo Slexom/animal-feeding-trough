5fb039f393f398983c5ccc310e4d1fde150f0d04
Carmelo Buscemi <b.carmelo.92@gmail.com>
2023-04-04 00:08:58 +0200

    MC 1.19.4

--- common/src/main/java/slexom/animal_feeding_trough/platform/common/AnimalFeedingTroughMod.java
+++ common/src/main/java/slexom/animal_feeding_trough/platform/common/AnimalFeedingTroughMod.java
@@ -13,6 +13,9 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.resource.featuretoggle.FeatureFlags;
+import net.minecraft.resource.featuretoggle.FeatureSet;
 import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
@@ -32,10 +35,10 @@ public class AnimalFeedingTroughMod {
 
     public static final Supplier<RegistrarManager> REGISTRIES = Suppliers.memoize(() -> RegistrarManager.get(MOD_ID));
 
-    public static final Registrar<Block> BLOCK_REGISTRAR = REGISTRIES.get().get(Registries.BLOCK);
-    public static final Registrar<Item> ITEM_REGISTRAR = REGISTRIES.get().get(Registries.ITEM);
-    public static final Registrar<BlockEntityType<?>> BLOCK_ENTITY_TYPE_REGISTRAR = REGISTRIES.get().get(Registries.BLOCK_ENTITY_TYPE);
-    public static final Registrar<ScreenHandlerType<?>> SCREEN_HANDLER_TYPE_REGISTRAR = REGISTRIES.get().get(Registries.SCREEN_HANDLER);
+    public static final Registrar<Block> BLOCK_REGISTRAR = REGISTRIES.get().get(RegistryKeys.BLOCK);
+    public static final Registrar<Item> ITEM_REGISTRAR = REGISTRIES.get().get(RegistryKeys.ITEM);
+    public static final Registrar<BlockEntityType<?>> BLOCK_ENTITY_TYPE_REGISTRAR = REGISTRIES.get().get(RegistryKeys.BLOCK_ENTITY_TYPE);
+    public static final Registrar<ScreenHandlerType<?>> SCREEN_HANDLER_TYPE_REGISTRAR = REGISTRIES.get().get(RegistryKeys.SCREEN_HANDLER);
 
     public static final Identifier REGISTRY_NAME = new Identifier(AnimalFeedingTroughMod.MOD_ID, "feeding_trough");
     public static RegistrySupplier<Block> FEEDING_TROUGH_BLOCK = BLOCK_REGISTRAR.register(REGISTRY_NAME, () -> new FeedingTroughBlock(AbstractBlock.Settings.of(Material.WOOD).sounds(BlockSoundGroup.WOOD).strength(0.2F).nonOpaque()));
@@ -47,8 +50,9 @@ public class AnimalFeedingTroughMod {
 
     public static void onInitializeClient() {
         MenuRegistry.registerScreenFactory(FEEDING_TROUGH_SCREEN_HANDLER.get(), FeedingTroughScreen::new);
-    }    public static RegistrySupplier<ScreenHandlerType<FeedingTroughScreenHandler>> FEEDING_TROUGH_SCREEN_HANDLER = SCREEN_HANDLER_TYPE_REGISTRAR.register(REGISTRY_NAME, () -> new ScreenHandlerType<FeedingTroughScreenHandler>(FeedingTroughScreenHandler::new));
+    }
 
+    public static RegistrySupplier<ScreenHandlerType<FeedingTroughScreenHandler>> FEEDING_TROUGH_SCREEN_HANDLER = SCREEN_HANDLER_TYPE_REGISTRAR.register(REGISTRY_NAME, () -> new ScreenHandlerType<FeedingTroughScreenHandler>(FeedingTroughScreenHandler::new, FeatureFlags.VANILLA_FEATURES));
 
 
     public static RegistrySupplier<BlockEntityType<FeedingTroughBlockEntity>> FEEDING_TROUGH_BLOCK_ENTITY = BLOCK_ENTITY_TYPE_REGISTRAR.register(REGISTRY_NAME, () -> BlockEntityType.Builder.create(FeedingTroughBlockEntity::new, FEEDING_TROUGH_BLOCK.get()).build(null));
--- quilt/src/main/java/slexom/animal_feeding_trough/platform/quilt/AnimalFeedingTroughClientModQuilt.java
+++ quilt/src/main/java/slexom/animal_feeding_trough/platform/quilt/AnimalFeedingTroughClientModQuilt.java
@@ -3,13 +3,14 @@ package slexom.animal_feeding_trough.platform.quilt;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.loader.api.minecraft.ClientOnly;
 import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
 import slexom.animal_feeding_trough.platform.common.AnimalFeedingTroughMod;
 
 public class AnimalFeedingTroughClientModQuilt implements ClientModInitializer {
 
     @Override
-    @Environment(EnvType.CLIENT)
+    @ClientOnly
     public void onInitializeClient(ModContainer mod) {
         AnimalFeedingTroughMod.onInitializeClient();
     }
