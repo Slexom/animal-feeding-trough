c6f502bda7fff5b8730a2fa9ffef8642273d78a0
Slexom <b.carmelo.92@gmail.com>
2023-11-20 22:17:02 +0100

    1.20.2 forge neoforge

--- neoforge/src/main/java/slexom/vf/animal_feeding_trough/neoforge/AnimalFeedingTroughModNeoForge.java
+++ neoforge/src/main/java/slexom/vf/animal_feeding_trough/neoforge/AnimalFeedingTroughModNeoForge.java
@@ -1,25 +1,21 @@
 package slexom.vf.animal_feeding_trough.neoforge;
 
-import dev.architectury.platform.forge.EventBuses;
 import net.minecraft.client.gui.screen.ingame.HandledScreens;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.DistExecutor;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 import slexom.animal_feeding_trough.platform.common.AnimalFeedingTroughMod;
 import slexom.animal_feeding_trough.platform.common.screen.FeedingTroughScreen;
 
 @Mod(AnimalFeedingTroughMod.MOD_ID)
 public class AnimalFeedingTroughModNeoForge {
 
-	public AnimalFeedingTroughModNeoForge() {
-		IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
-		EventBuses.registerModEventBus(AnimalFeedingTroughMod.MOD_ID, eventBus);
-		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> (DistExecutor.SafeRunnable) () -> eventBus.addListener(this::setupClient));
+	public AnimalFeedingTroughModNeoForge(IEventBus modEventBus) {
+ 		modEventBus.addListener(this::setupClient);
 
 		AnimalFeedingTroughMod.onInitialize();
 	}
